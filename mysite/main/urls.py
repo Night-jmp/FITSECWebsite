a1958fbb74be041a455582329549260c3c2fbcac
BuildTools <unconfigured@null.spigotmc.org>
2020-05-07 22:08:33 -0400

    wow

---
+++ mysite/main/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'main'  # here for namespacing of urls.
+
+urlpatterns = [
+  path("", views.homepage, name="homepage"),
+  path("register/", views.register, name="register"),
+  path("logout", views.logout_request, name="logout"),
+  path("login", views.login_request, name="login"),
+  
+  path("writeups", views.writeups, name="writeups"),
+  path("contact", views.contact, name="contact"),
+  path("about", views.about, name="about"),
+  path("getinvolved", views.getinvolved, name="getinvolved"),
+]
